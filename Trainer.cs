d0fe6c1ab4766b9d2bab75d2dbf90f20851b9731
rodrigocitadin <citadinrodrigo@gmail.com>
2023-11-27 01:46:18 -0300

    change to simple responsability and add more offsets

--- Trainer.cs
+++ Trainer.cs
@@ -6,30 +6,29 @@ namespace AcTrainer.Trainer;
 public class Trainer
 {
     Mem memory = new();
+
     IntPtr playerOffset = 0x17E0A8;
-    IntPtr ammoOffset = 0x140;
+    IntPtr rifleAmmoOffset = 0x140;
+    IntPtr healthOffset = 0xEC;
+    IntPtr granadeOffset = 0x144;
+    IntPtr armorOffset = 0xF0;
 
-    public void WriteAmmo()
+    public void InfinityRiffleAmmo(Process proc)
     {
-        Process proc;
-
         try
         {
             proc = Process.GetProcessesByName("ac_client")[0];
         }
-        catch (Exception _err)
+        catch (Exception _ex)
         {
             return;
         }
 
-        IntPtr mainModuleBaseAddress = proc.MainModule.BaseAddress;
+        IntPtr mainModuleBaseAddress = proc.MainModule!.BaseAddress;
 
         IntPtr player = memory.readPointer(proc.Handle, mainModuleBaseAddress + playerOffset);
-        IntPtr ammo = memory.readPointer(proc.Handle, player + ammoOffset);
+        IntPtr ammo = memory.readPointer(proc.Handle, player + rifleAmmoOffset);
 
-        while (true)
-        {
-            memory.writeBytes(proc.Handle, player + ammoOffset, BitConverter.GetBytes(20));
-        }
+        memory.writeBytes(proc.Handle, player + rifleAmmoOffset, BitConverter.GetBytes(20));
     }
 }
